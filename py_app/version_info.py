abc3844b1dc13d605a7d2a680b3936ac8c957e94
toma <bs.kougyou@gmail.com>
2025-02-25 14:03:54 +0900

    バージョンを1.0.1に更新

--- py_app/version_info.py
+++ py_app/version_info.py
@@ -1,7 +1,7 @@
 VSVersionInfo(
     ffi=FixedFileInfo(
-        filevers=(1, 0, 0, 0),
-        prodvers=(1, 0, 0, 0),
+        filevers=(1, 0, 1, 0),
+        prodvers=(1, 0, 1, 0),
         mask=0x3F,
         flags=0x0,
         OS=0x40004,
@@ -17,12 +17,12 @@ VSVersionInfo(
                     [
                         StringStruct("CompanyName", "Your Company"),
                         StringStruct("FileDescription", "Directory Size Viewer"),
-                        StringStruct("FileVersion", "1.0.0"),
+                        StringStruct("FileVersion", "1.0.1"),
                         StringStruct("InternalName", "Directory_Size_Viewer"),
                         StringStruct("LegalCopyright", "Copyright (c) 2024"),
                         StringStruct("OriginalFilename", "Directory_Size_Viewer.exe"),
                         StringStruct("ProductName", "Directory Size Viewer"),
-                        StringStruct("ProductVersion", "1.0.0"),
+                        StringStruct("ProductVersion", "1.0.1"),
                     ],
                 )
             ]
@@ -35,4 +35,4 @@ VSVersionInfo(
 バージョン情報管理モジュール
 """
 
-VERSION = "1.0.0"  # メジャー.マイナー.パッチ
+VERSION = "1.0.1"  # メジャー.マイナー.パッチ
